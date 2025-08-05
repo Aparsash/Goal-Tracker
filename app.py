c7f8f38f388615b8f0aa3b615c3519f9f2a89aff
Parsa SH <aparsash.dev@gmail.com>
2025-08-05 11:09:45 +0330

    add databse url

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 import os
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'fallback-secret')
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, "site.db")}'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 @app.before_request
